2e0a6933771aa3c694ba8039ef4bbaa79c805932
n0tea <artnotea04@gmail.com>
2024-12-01 22:34:19 +0300

    сделал новый юи

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,18 +2,22 @@ using UnityEngine;
 using Cinemachine;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private RectTransform _menuRectTransform;
     [SerializeField] private RectTransform _trainsRectTransform;
     [SerializeField] private RectTransform _enterprisesRectTransform;
+    [SerializeField] private RectTransform _trainsSelectRectTransform;
 
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private Color _solidColor;
     [SerializeField] private CinemachineVirtualCamera _cinemachineStartCamera;
     [SerializeField] private CinemachineVirtualCamera _cinemachineMainCamera;
     [SerializeField] private float _transitionDuration = 1f;
+
+    [SerializeField] private Image _image;
 
     private bool _isInMainScene = false;
 
@@ -25,8 +29,11 @@ public class UIManager : MonoBehaviour
         _cinemachineStartCamera.Priority = 10;
         _cinemachineMainCamera.Priority = 0;
 
-        _trainsRectTransform.anchoredPosition = new Vector2(-Screen.width, 0);
+        _trainsSelectRectTransform.anchoredPosition = new Vector2(-Screen.width, 0);
+        _trainsRectTransform.anchoredPosition = new Vector2(Screen.width, 0); // мб *2
         _enterprisesRectTransform.anchoredPosition = new Vector2(Screen.width, 0);
+
+        _image.enabled = false;
     }
 
     public void OnStartButtonPressed()
@@ -37,6 +44,7 @@ public class UIManager : MonoBehaviour
 
         // Завершаем текущие анимации интерфейса и цвета, чтобы избежать наложений
         _menuRectTransform.DOKill();
+        _trainsSelectRectTransform.DOKill();
         _trainsRectTransform.DOKill();
         _mainCamera.DOKill();
 
@@ -46,6 +54,7 @@ public class UIManager : MonoBehaviour
 
         // Анимация интерфейса
         _menuRectTransform.DOAnchorPos(new Vector2(Screen.width, 0), _transitionDuration);
+        _trainsSelectRectTransform.DOAnchorPos(new Vector2(Screen.width, 0), _transitionDuration);
         _trainsRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
 
         // Переключение камеры
@@ -53,7 +62,7 @@ public class UIManager : MonoBehaviour
         _cinemachineMainCamera.Priority = 10;
     }
 
-    public void ReturnFromModels()
+    public void ReturnFromModelsToMain()
     {
         if (!_isInMainScene) return;
 
@@ -61,11 +70,13 @@ public class UIManager : MonoBehaviour
 
         // Завершаем текущие анимации интерфейса и цвета
         _menuRectTransform.DOKill();
+        _trainsSelectRectTransform.DOKill();
         _trainsRectTransform.DOKill();
         _mainCamera.DOKill();
 
         // Анимация возврата камеры
         _menuRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
+        _trainsSelectRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
         _trainsRectTransform.DOAnchorPos(new Vector2(-Screen.width, 0), _transitionDuration);
 
         // Включаем solidColor только после завершения анимации
@@ -77,7 +88,33 @@ public class UIManager : MonoBehaviour
         _cinemachineMainCamera.Priority = 0;
     }
 
-    public void OpenEnterprisesVindow()
+    public void ReturnFromModelsToSelect()
+    {
+        if (!_isInMainScene) return;
+
+        _isInMainScene = false;
+
+        // Завершаем текущие анимации интерфейса и цвета
+        _menuRectTransform.DOKill();
+        _trainsSelectRectTransform.DOKill();
+        _trainsRectTransform.DOKill();
+        _mainCamera.DOKill();
+
+        // Анимация возврата камеры
+        _menuRectTransform.DOAnchorPos(new Vector2(Screen.width, 0), _transitionDuration);
+        _trainsSelectRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
+        _trainsRectTransform.DOAnchorPos(new Vector2(-Screen.width, 0), _transitionDuration);
+
+        // Включаем solidColor только после завершения анимации
+        _mainCamera.DOColor(_solidColor, _transitionDuration)
+            .OnComplete(() => _mainCamera.clearFlags = CameraClearFlags.SolidColor);
+
+        // Переключение камеры
+        _cinemachineStartCamera.Priority = 10;
+        _cinemachineMainCamera.Priority = 0;
+    }
+
+    public void OpenEnterprisesWindow()
     {
         // Завершаем текущие анимации интерфейса
         _menuRectTransform.DOKill();
@@ -86,6 +123,27 @@ public class UIManager : MonoBehaviour
         _menuRectTransform.DOAnchorPos(new Vector2(-Screen.width, 0), _transitionDuration);
         _enterprisesRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
     }
+    public void OpenTrainsSelectWindow()
+    {
+        // Завершаем текущие анимации интерфейса
+        _menuRectTransform.DOKill();
+        _trainsSelectRectTransform.DOKill();
+
+        _menuRectTransform.DOAnchorPos(new Vector2(Screen.width, 0), _transitionDuration);
+        _trainsSelectRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
+    }
+
+    /*public void FromModelOpenTrainsSelectWindow()
+    {
+        // Завершаем текущие анимации интерфейса
+        //_menuRectTransform.DOKill();
+        _trainsRectTransform.DOKill();
+        _trainsSelectRectTransform.DOKill();
+
+        _trainsRectTransform.DOAnchorPos(new Vector2(-Screen.width, 0), _transitionDuration);
+        _trainsSelectRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
+    }*/
+
 
     public void ReturnFromEnterprises()
     {
@@ -93,8 +151,44 @@ public class UIManager : MonoBehaviour
         _menuRectTransform.DOKill();
         _enterprisesRectTransform.DOKill();
 
-        _menuRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
+        _image.enabled = true;
+        // белый фон во время анимации
+        _menuRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration).OnComplete(() => { _image.enabled = false; });
         _enterprisesRectTransform.DOAnchorPos(new Vector2(Screen.width, 0), _transitionDuration);
+    }
+
+    public void ReturnFromEnterprisesToSelect()
+    {
+        // Завершаем текущие анимации интерфейса
+        _trainsSelectRectTransform.DOKill();
+        _enterprisesRectTransform.DOKill();
+
+        
+        
+        _trainsSelectRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
+        _enterprisesRectTransform.DOAnchorPos(new Vector2(Screen.width, 0), _transitionDuration);
+    }
+
+    public void ReturnFromTrainsSelect()
+    {
+        // Завершаем текущие анимации интерфейса
+        _menuRectTransform.DOKill();
+        _trainsSelectRectTransform.DOKill();
+
+        _image.enabled = true;
+        // белый фон во время анимации
+        _menuRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration).OnComplete(() => { _image.enabled = false; });
+        _trainsSelectRectTransform.DOAnchorPos(new Vector2(-Screen.width, 0), _transitionDuration);
+    }
+
+    public void ReturnFromTrainsSelectToEnterprises()
+    {
+        // Завершаем текущие анимации интерфейса
+        _enterprisesRectTransform.DOKill();
+        _trainsSelectRectTransform.DOKill();
+
+        _enterprisesRectTransform.DOAnchorPos(Vector2.zero, _transitionDuration);
+        _trainsSelectRectTransform.DOAnchorPos(new Vector2(-Screen.width, 0), _transitionDuration);
     }
 
     public void LoadEnterpriseScene()
